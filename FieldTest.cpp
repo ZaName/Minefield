aac2edf959ea17d3aee16b5ba4eb136c6a9df3a3
ZaName <ivyhillcaleb@gmail.com>
2016-09-26 11:49:53 -0700

    Found Bugs

--- FieldTest.cpp
+++ FieldTest.cpp
@@ -22,3 +22,41 @@ TEST(FieldTest, placeMineInBounds)
 	minefield.placeMine(4,5);
 	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
 }
+
+TEST(FieldTest, isGetOutOfBounds)
+{
+	Field minefield(50);
+
+	bool failed = false;
+	try {
+		minefield.get(50,0);
+	} catch (std::exception ex) {
+		failed = true;
+	}
+	
+	ASSERT_TRUE(failed);
+}
+
+TEST(FieldTest, isSafeOutOfBounds)
+{
+	Field minefield(50);
+
+	bool failed = false;
+	try {
+		minefield.isSafe(50,0);
+	} catch (std::exception ex) {
+		failed = true;
+	}
+	
+	ASSERT_TRUE(failed);
+}
+
+TEST(FieldTest, isRevealAdj)
+{
+	Field minefield(50);
+
+	minefield.placeMine(0,0);
+	minefield.revealAdjacent(0, 1);
+	
+	ASSERT_EQ( MINE_SHOWN, minefield.get(0,0) );
+}
--- buglog.md
+++ buglog.md
@@ -1,1 +1,4 @@
 # BUG LOG
+
+1. isSafe and get both have errors when the col or row is one too big because the inBounds function does not work right
+2. revealAdjacent has an issue where it does not reveal the adjacent bombs, when placing a bomb at (0,0) and revealing at (0,1) it should reveal the bomb but it is not
